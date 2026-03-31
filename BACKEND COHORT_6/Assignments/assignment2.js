9e53cb808bd9bfdecaacd202230d929a11c39496
olatomide-web <joshboris2020@gmail.com>
2026-03-31 15:21:54 +0100

    assignment submitted

--- BACKEND COHORT_6/Assignments/assignment2.js
+++ BACKEND COHORT_6/Assignments/assignment2.js
@@ -1,27 +1,34 @@
-
+//QUESTION 1
 const str = "Nigeria is awesome";
+
 let extractNigeria = str.substring(0,7)
 console.log(extractNigeria)
 
-let extractAwesome = str.slice(11,18)
+let extractAwesome = str.slice(11)
 console.log(extractAwesome)
 
+let getIs = str.slice(8,10)
+console.log(getIs)
+
+//QUESTION 2
 const names = "John, Jane, Doe";
 let splitnameArray = names.split(",")
 console.log(splitnameArray)
 
-let joinnameArray = names.split(" ")
+let joinnameArray = names.trim(" ")
 console.log(joinnameArray)
 
 const path = "/user/home/docs";
 let splitPath = path.split("/")
 console.log(splitPath)
 
-let getHome = path.split("home")
+let getHome = path.split("/")[2]
 console.log(getHome)
 
 let getHomeIndex = path.indexOf("home")
 console.log(getHomeIndex)
+
+//QUESTION 3
 
 const email = "user@example.com";
 let getUsername  = email.split(0,4)
@@ -32,3 +39,4 @@ console.log(endwith)
 
 let replaceAt = email.replace("@", "AT")
 console.log(replaceAt)
+
